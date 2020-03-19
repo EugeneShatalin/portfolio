a9e9ce35e1f3fa820eb030737dcb493ebee0dd65
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-19 15:46:07 +0600

    Updates

---
+++ precache-manifest.72df3a379e86977d6d533e7bbada8d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a424fd979115282b908389b4b045c2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6c8808f1b2082fc68d7b",
+    "url": "/portfolio/static/css/main.24ac1f7a.chunk.css"
+  },
+  {
+    "revision": "e6a172ba5d5103084614",
+    "url": "/portfolio/static/js/2.b8750d74.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.b8750d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8808f1b2082fc68d7b",
+    "url": "/portfolio/static/js/main.00e89d0c.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
