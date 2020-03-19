caa8bf8c1f1777375d1223903a5fbc3bad530093
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-19 15:37:27 +0600

    first commit

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Slogan.module.css';
+
+function Skills() {
+  return (
+    <div className={s.slogan}>
+
+    </div>
+  );
+}
+
+export default Skills;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Contacts.module.css';
+
+function Contacts() {
+  return (
+    <div className={s.slogan}>
+
+    </div>
+  );
+}
+
+export default Contacts;
