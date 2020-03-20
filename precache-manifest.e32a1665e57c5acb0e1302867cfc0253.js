23efd490c0e6fead17c9bf4ccc13c31ea737a5d7
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-21 00:08:30 +0600

    Updates

---
+++ precache-manifest.e32a1665e57c5acb0e1302867cfc0253.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a213315d5aaf218566e55bd98c33e900",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eeaf760e768de81a3aa3",
+    "url": "/portfolio/static/css/main.292eb617.chunk.css"
+  },
+  {
+    "revision": "a0b26af179f544646b10",
+    "url": "/portfolio/static/js/2.beec77e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.beec77e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeaf760e768de81a3aa3",
+    "url": "/portfolio/static/js/main.86143b20.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "5edde5fbfe78c2ec5db07a62a2518fde",
+    "url": "/portfolio/static/media/React.5edde5fb.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.697ad8fa0053f7010abea16aae5ba229.js"
+  "/portfolio/precache-manifest.e32a1665e57c5acb0e1302867cfc0253.js"
 );
 
 self.addEventListener('message', (event) => {
